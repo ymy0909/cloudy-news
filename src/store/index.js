39fdacedb7e7ccd2669a0113e3b68e1d86e5e87b
admin <139234848@qq.com>
2019-10-19 11:24:41 +0800

    jiaojie

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import reducer from './reducer'
 import { createStore,applyMiddleware } from 'redux'
 
-const store = createStore(reducerName)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store
