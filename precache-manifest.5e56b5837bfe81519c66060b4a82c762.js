c267588722a34ceb9af8d401444684265e3a53a5
yangmingyu <yangmingyu@oakshanghai.com>
2019-10-24 17:03:43 +0800

    Updates

---
+++ precache-manifest.5e56b5837bfe81519c66060b4a82c762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb887391d1c3978e64ea8ddf4a401e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c18f77d144ff2a37265",
+    "url": "/static/css/2.f748834c.chunk.css"
+  },
+  {
+    "revision": "e87369251ac47bd70f86",
+    "url": "/static/css/main.3569ea4c.chunk.css"
+  },
+  {
+    "revision": "3c18f77d144ff2a37265",
+    "url": "/static/js/2.24c11cd6.chunk.js"
+  },
+  {
+    "revision": "e87369251ac47bd70f86",
+    "url": "/static/js/main.31bf69c8.chunk.js"
+  },
+  {
+    "revision": "2b7cbd82c41764ac0eed",
+    "url": "/static/js/runtime-main.5bd14bb5.js"
+  }
+]);
