6b973c8cdf729b6995667ce58d1476a8ed94b369
yangmingyu <yangmingyu@oakshanghai.com>
2019-10-24 17:35:40 +0800

    Updates

---
+++ precache-manifest.bd3a26978d1b5cfbb9fe50e52a5f0349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43452413699f4fcd4d78cc770b4f004",
+    "url": "/cloudy-news/index.html"
+  },
+  {
+    "revision": "34d89e821bc6ac5e3417",
+    "url": "/cloudy-news/static/css/2.f748834c.chunk.css"
+  },
+  {
+    "revision": "5ca0a210e663704ff018",
+    "url": "/cloudy-news/static/css/main.3569ea4c.chunk.css"
+  },
+  {
+    "revision": "34d89e821bc6ac5e3417",
+    "url": "/cloudy-news/static/js/2.0f6236ba.chunk.js"
+  },
+  {
+    "revision": "5ca0a210e663704ff018",
+    "url": "/cloudy-news/static/js/main.a1cb3762.chunk.js"
+  },
+  {
+    "revision": "ef79b102a529388f5fc9",
+    "url": "/cloudy-news/static/js/runtime-main.f7ef7bc0.js"
+  }
+]);
