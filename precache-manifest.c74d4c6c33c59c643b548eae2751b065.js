1d459eaca0b2dbd4811e02e9250f3452b2f5c710
yangmingyu <yangmingyu@oakshanghai.com>
2019-10-24 11:43:10 +0800

    Updates

---
+++ precache-manifest.c74d4c6c33c59c643b548eae2751b065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3e30cd34a09efd1f486e648692c973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c18f77d144ff2a37265",
+    "url": "/static/css/2.f748834c.chunk.css"
+  },
+  {
+    "revision": "266ba7546979396f6990",
+    "url": "/static/css/main.3569ea4c.chunk.css"
+  },
+  {
+    "revision": "3c18f77d144ff2a37265",
+    "url": "/static/js/2.24c11cd6.chunk.js"
+  },
+  {
+    "revision": "266ba7546979396f6990",
+    "url": "/static/js/main.3e956fd9.chunk.js"
+  },
+  {
+    "revision": "2b7cbd82c41764ac0eed",
+    "url": "/static/js/runtime-main.5bd14bb5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ymy0909/cloudy-news/precache-manifest.7487dd6d51d784fbf4da05fe450e0340.js"
+  "/precache-manifest.c74d4c6c33c59c643b548eae2751b065.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ymy0909/cloudy-news/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
