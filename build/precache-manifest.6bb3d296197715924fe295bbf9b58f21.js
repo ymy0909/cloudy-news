fa58639ec5153241fb58a38c48bba36a48b2d6b6
yangmingyu <yangmingyu@oakshanghai.com>
2019-10-24 10:27:40 +0800

    'addbuild'

---
+++ build/precache-manifest.6bb3d296197715924fe295bbf9b58f21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41a584e1d50fa1923dc3f4a383f85ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c18f77d144ff2a37265",
+    "url": "/static/css/2.f748834c.chunk.css"
+  },
+  {
+    "revision": "546c39a01a2d5c04975b",
+    "url": "/static/css/main.3569ea4c.chunk.css"
+  },
+  {
+    "revision": "3c18f77d144ff2a37265",
+    "url": "/static/js/2.24c11cd6.chunk.js"
+  },
+  {
+    "revision": "546c39a01a2d5c04975b",
+    "url": "/static/js/main.7e6a21f3.chunk.js"
+  },
+  {
+    "revision": "2b7cbd82c41764ac0eed",
+    "url": "/static/js/runtime-main.5bd14bb5.js"
+  }
+]);
